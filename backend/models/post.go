5084e1a3ecb6b66eed73d400d1a3207f58befa59
nyunja <poptiff@gmail.com>
2025-02-05 16:02:36 +0300

    update: add raw categories field from client side

--- backend/models/post.go
+++ backend/models/post.go
@@ -1,14 +1,15 @@
 package models
 
 type Post struct {
-	ID         int        `json:"id"`
-	UserID     int        `json:"user_id"`
-	Username   string     `json:"username"`
-	Title      string     `json:"title"`
-	Content    string     `json:"content"`
-	CreatedAt  string     `json:"created_at"`
-	Likes      int        `json:"likes"`
-	Dislikes   int        `json:"dislikes"`
-	Comments   []Comment  `json:"comments"`
-	Categories []Category `json:"categories,omitempty"` // Add this field
+	ID            int        `json:"id"`
+	UserID        int        `json:"user_id"`
+	Username      string     `json:"username"`
+	Title         string     `json:"title"`
+	Content       string     `json:"content"`
+	CreatedAt     string     `json:"created_at"`
+	Likes         int        `json:"likes"`
+	Dislikes      int        `json:"dislikes"`
+	Comments      []Comment  `json:"comments"`
+	RawCategories []int      `json:"raw_categories"`
+	Categories    []Category `json:"categories,omitempty"` // Add this field
 }
