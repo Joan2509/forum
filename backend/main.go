47385541fff7f97e35bb8d1c3cb5b0d6519017d7
nyunja <poptiff@gmail.com>
2025-02-06 01:48:12 +0300

    feat: add createCcommentLikesDislikes function getCommentLikesDislikes function for the comments

--- backend/main.go
+++ backend/main.go
@@ -62,7 +62,8 @@ func main() {
 	protectedMux.HandleFunc("/api/posts", handlers.GetPostsHandler)
 	protectedMux.HandleFunc("/api/posts/create", handlers.CreatePostHandler)
 	protectedMux.HandleFunc("/api/comments", handlers.CreateCommentHandler)
-	protectedMux.HandleFunc("/api/likes", handlers.CreateLikeDislikeHandler)
+	protectedMux.HandleFunc("/api/comments/likes", handlers.CreateCommentLikeDislikeHandler)
+	protectedMux.HandleFunc("/api/posts/likes", handlers.CreateLikeDislikeHandler)
 	protectedMux.HandleFunc("/api/auth/status", handlers.AuthStatusHandler)
 
 	http.Handle("/api/protected/", middleware.AuthMiddleware(http.StripPrefix("/api/protected", protectedMux)))
