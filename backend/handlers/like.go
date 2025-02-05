f411d76d72e7332450aa042f3b41ef82ec227cbd
nyunja <poptiff@gmail.com>
2025-02-06 01:49:06 +0300

    feat: implement handler for comment likes

--- backend/handlers/like.go
+++ backend/handlers/like.go
@@ -31,3 +31,26 @@ func CreateLikeDislikeHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(map[string]string{"message": "Like/Dislike created successfully"})
 }
+
+func CreateCommentLikeDislikeHandler(w http.ResponseWriter, r *http.Request) {
+	userId, ok := middleware.GetUserID(r)
+	if !ok {
+		http.Error(w, "Unauthorized: No user ID", http.StatusUnauthorized)
+		return
+	}
+	var like models.LikeDislike
+	like.UserID = userId
+	err := json.NewDecoder(r.Body).Decode(&like)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+	err = database.CreateCommentLikeDislike(like)
+	if err != nil {
+		http.Error(w, "Failed to like/dislike", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Like/Dislike created successfully"})
+}
